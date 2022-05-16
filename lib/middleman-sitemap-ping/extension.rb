198e92bf903befe77d4cf64a1b98c0e93e0ac96a
Matias Garcia Isaia <mgarcia@manas.tech>
2022-05-16 12:23:01 -0300

    Remove deprecated Bing endpoint
    See https://blogs.bing.com/webmaster/may-2022/Spring-cleaning-Removed-Bing-anonymous-sitemap-submission

--- lib/middleman-sitemap-ping/extension.rb
+++ lib/middleman-sitemap-ping/extension.rb
@@ -4,8 +4,7 @@ module Middleman
   module SitemapPing
     class Extension < Middleman::Extension
       SERVICES = {
-        google: 'https://www.google.com/webmasters/tools/ping?sitemap=%SITEMAP_URL%',
-        bing:   'https://bing.com/webmaster/ping.aspx?sitemap=%SITEMAP_URL%'
+        google: 'https://www.google.com/webmasters/tools/ping?sitemap=%SITEMAP_URL%'
       }
 
       SERVICES.each_key do |service|
